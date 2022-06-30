2f54b74cafdd6bb93e71b19d27c79c70656dae8b
abigael <s2200643@student.unklab.ac.id>
2022-07-01 04:51:11 +0800

    junk

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,11 +1,9 @@
 import GetData from "../../components kecil/get data";
-import Header from "../../components kecil/header";
 import Image from "../../components kecil/image";
 
 export default function Content() {
     return(
     <tbody>
-            <Header/>
             <Image/>
             <GetData/>
     </tbody>
